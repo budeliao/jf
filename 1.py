7a46187cc8a444a47eb3331c345f0414e1306ec5
wk980821 <15866881931@163.com>
2019-04-01 21:35:53 +0800

    请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交

--- 1.py
+++ 1.py
@@ -1,3 +1,21 @@
 print(1)
 print(2)
 print('坤哥牛逼')
+
+
+def quick_sort(li):
+    for i in range(len(li), 1, -1):
+        swap_num = 0
+        for j in range(i-1):
+            if li[j] > li[j+1]:
+                li[j], li[j+1] = li[j+1], li[j]
+                swap_num += 1
+        if swap_num == 0:
+            return
+
+
+
+if __name__ == '__main__':
+    li = [9,8,7,6,5,4,3,2,1,2,3,4,6,8,45,63]
+    quick_sort(li)
+    print(li)
